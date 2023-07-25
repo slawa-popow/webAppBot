ee00f8a3a0cd55fcffc507dcf9106939341a8585
slawa-popow <110kwt@gmail.com>
2023-07-25 16:36:41 +0300

    0

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -10,7 +10,7 @@ mainRouter.post('/getCategory', mainController.getAllCategory);
 mainRouter.post('/getTenProd', mainController.getTenProd);
 mainRouter.post('/getUsid', mainController.getUserId);
 mainRouter.post('/addProductOnBasket', mainController.addToBasket)
-mainRouter.post('/removeProductFromBasket', mainController.removeFromBasket) 
+mainRouter.post('/removeProductFromBasket', mainController.removeFromBasket)
 
 
 export { mainRouter }
