4bf7f75483925903180a40b67e757e074079cb2d
slawa-popow <110kwt@gmail.com>
2023-07-25 17:37:58 +0300

    f3v

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { mainController } from "./MainController";
-import { validateUser, } from "../middlewares/validateUser";
+import { validateUser, validUsr} from "../middlewares/validateUser";
 
 
 const mainRouter = Router(); 
 
-mainRouter.get('/:id',validateUser, mainController.getIndexPage);
+mainRouter.get('/main/:id',validateUser, mainController.getIndexPage);
 mainRouter.post('/getCategory', mainController.getAllCategory);
 mainRouter.post('/getTenProd', mainController.getTenProd);
-mainRouter.post('/getUsid', mainController.getUserId); 
+mainRouter.post('/getUsid', validUsr, mainController.getUserId); 
 mainRouter.post('/addProductOnBasket', mainController.addToBasket)
 mainRouter.post('/removeProductFromBasket', mainController.removeFromBasket)
 
