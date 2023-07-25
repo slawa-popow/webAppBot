be44090d02291c5d28cc43c5592b5e9d924cde1c
slawa-popow <110kwt@gmail.com>
2023-07-25 16:45:10 +0300

    a77

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { mainController } from "./MainController";
-import { validateUser, } from "../middlewares/validateUser";
+// import { validateUser, } from "../middlewares/validateUser";
 
 
 const mainRouter = Router(); 
 
-mainRouter.get('/:id', validateUser, mainController.getIndexPage);
+mainRouter.get('/:id', mainController.getIndexPage);
 mainRouter.post('/getCategory', mainController.getAllCategory);
 mainRouter.post('/getTenProd', mainController.getTenProd);
 mainRouter.post('/getUsid', mainController.getUserId);
