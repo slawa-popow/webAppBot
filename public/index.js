3309cb0811421cbb948a1e1eb0017f655a9a447b
slawa-popow <110kwt@gmail.com>
2023-08-12 22:12:02 +0300

    гребаный кеш...нашел способ вытащить остатки модификаций товара из мс.....

--- public/index.js
+++ public/index.js
@@ -11,7 +11,7 @@ import { TicketMan } from "./src/TicketMan";
 
 const prodURL = 'https://web-app-bot-b.vercel.app/' 
 const devURL = '/';
-const URL = devURL;
+const URL = distURL;
 
 
 window.Telegram.WebApp.ready();
