ec239d902f2944608e76944e8e926384fc516aa2
slawa-popow <110kwt@gmail.com>
2023-09-11 21:15:49 +0300

    fhj

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -1,21 +1,21 @@
 import { Router } from "express";
 import { mainController } from "./MainController";
-// import { validUsr, validateUser } from "../middlewares/validateUser";
+import {  validateUser } from "../middlewares/validateUser";
 
 
 
 const mainRouter = Router(); 
 
-mainRouter.get('/', mainController.getIndexPage);
-mainRouter.get('/basket',   mainController.getBasketPage);
-mainRouter.post('/getCategory', mainController.getAllCategory);
-mainRouter.post('/getTenProd',  mainController.getTenProd);
+mainRouter.get('/', validateUser, mainController.getIndexPage);
+mainRouter.get('/basket',  mainController.getBasketPage);
+mainRouter.post('/getCategory',  mainController.getAllCategory);
+mainRouter.post('/getTenProd', mainController.getTenProd);
 mainRouter.post('/getUsid',  mainController.getUserId); 
 // mainRouter.put('/addProductOnBasket', validUsr, mainController.addToBasket);
 mainRouter.put('/removeProductFromBasket',  mainController.removeFromBasket);
 mainRouter.put('/fillUserBasket',  mainController.fillUserBasket)
-mainRouter.post('/getFindByCharacteristics', mainController.searchByCharacts);
-mainRouter.post('/getCalculate', mainController.getCalculate);
+mainRouter.post('/getFindByCharacteristics',  mainController.searchByCharacts);
+mainRouter.post('/getCalculate',  mainController.getCalculate);
 mainRouter.post('/getUsrBasket',  mainController.getUsrBasket);
 mainRouter.post('/deleteProduct',  mainController.delProduct);
 
